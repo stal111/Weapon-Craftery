eaf882d38beb114ce57e454ec8ccc8d369e7dd8e
stal111 <stal111@gmx.de>
2019-06-25 17:38:09 +0200

    Initial commit

--- src/main/java/com/stal111/weapon_craftery/Main.java
+++ src/main/java/com/stal111/weapon_craftery/Main.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.stal111.weapon_craftery;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -18,14 +18,15 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(Main.MOD_ID)
+public class Main
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+	
+	public static final String MOD_ID = "weapon_craftery";
+	
+    private static final Logger LOGGER = LogManager.getLogger(Main.MOD_ID);
 
-    public ExampleMod() {
+    public Main() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
